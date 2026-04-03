eb403cc28da886a77fd6e5f73d0ee1aaea273695
john12321 <johnomeara72@gmail.com>
2026-04-03 20:32:29 +0100

    fix: backlink

--- src/components/BackLink.tsx
+++ src/components/BackLink.tsx
@@ -12,31 +12,25 @@ export const BackLink: React.FC<BackLinkProps> = ({
   disabled = false
 }) => {
   return (
-    <button
-      type="button"
-      className="nhsuk-back-link_link"
-      onClick={onClick}
-      disabled={disabled}
-      style={{
-        background: "none",
-        border: "none",
-        padding: 0,
-        margin: 0,
-        display: "flex",
-        alignItems: "center"
-      }}
-    >
-      <svg
-        className="nhsuk-icon nhsuk-icon__chevron-left"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        aria-hidden="true"
-        height="24"
-        width="24"
+    <div className="nhsuk-back-link">
+      <button
+        type="button"
+        className="nhsuk-back-link__link"
+        onClick={onClick}
+        disabled={disabled}
       >
-        <path d="M8.5 12c0-.3.1-.5.3-.7l5-5c.4-.4 1-.4 1.4 0s.4 1 0 1.4L10.9 12l4.3 4.3c.4.4.4 1 0 1.4s-1 .4-1.4 0l-5-5c-.2-.2-.3-.4-.3-.7z"></path>
-      </svg>
-      <span style={{ marginLeft: "0px", fontSize: "16px" }}>{children}</span>
-    </button>
+        <svg
+          className="nhsuk-icon nhsuk-icon__chevron-left"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          aria-hidden="true"
+          height="24"
+          width="24"
+        >
+          <path d="M8.5 12c0-.3.1-.5.3-.7l5-5c.4-.4 1-.4 1.4 0s.4 1 0 1.4L10.9 12l4.3 4.3c.4.4.4 1 0 1.4s-1 .4-1.4 0l-5-5c-.2-.2-.3-.4-.3-.7z"></path>
+        </svg>
+        {children}
+      </button>
+    </div>
   );
 };
